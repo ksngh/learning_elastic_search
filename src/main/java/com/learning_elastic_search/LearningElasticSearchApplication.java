4ffb2d36196c930a5ed56269ce02e4410071bc19
Seongho <chzhqk98@naver.com>
2025-08-04 09:49:13 +0900

    first commit

---
+++ src/main/java/com/learning_elastic_search/LearningElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.learning_elastic_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningElasticSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningElasticSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning_elastic_search/LearningElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning_elastic_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningElasticSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
